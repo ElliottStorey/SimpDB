0d4d32baa6ccafdd0f4e7c75b0ff5dd15e285bd0
Elliott Storey <70775866+ElliottStorey@users.noreply.github.com>
2022-05-02 14:49:48 -0500

    Update usage.py

--- usage.py
+++ usage.py
@@ -1,3 +1,25 @@
+#### Import Module
 from simpdb import SimpDB
-mydb = SimpDB("./mydatabase.sdb")
-mydb.create({"username":"ElliottStorey", "password":"password123!"})
+
+
+#### Initialize Database
+myDatabase = SimpDB("./myDatabase.sdb")
+
+
+#### Set Key Values
+# Create Username and Password
+myDatabase.set("username", "ElliottStorey")
+myDatabase.set("password", 314159)
+
+# Update Password
+myDatabase.set("password", "Password123!")
+
+
+#### Read Key Values
+username = myDatabase.read("username")
+print(username)
+
+
+#### Delete Key Values
+myDatabase.delete("username")
+myDatabase.delete("password")
